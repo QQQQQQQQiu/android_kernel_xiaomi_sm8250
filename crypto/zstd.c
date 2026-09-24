eb0cccadf36e55b07f6a9dae2d0e6979967a2220
Qiu <841371467@qq.com>
2026-09-24 11:06:47 +0800

    zram: set default zstd compression level to 10

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -4,8 +4,9 @@
  * Copyright (c) 2017-present, Facebook, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 as published by the Free
+ * Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -23,7 +24,7 @@
 #include <crypto/internal/scompress.h>
 
 
-#define ZSTD_DEF_LEVEL	1
+#define ZSTD_DEF_LEVEL	10
 
 struct zstd_ctx {
 	zstd_cctx *cctx;
